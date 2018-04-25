f4e82057cf5ed28a6833e4ad6f3669b2d2150e55
MaxRobinson <maxrgit@gmail.com>
2018-04-25 00:01:04 -0400

    Update of reults processing to plot averages all on a graph together

--- DistQL/results/processResults.py
+++ DistQL/results/processResults.py
@@ -9,58 +9,59 @@ import matplotlib.pyplot as plt
 np.set_printoptions(threshold=np.nan)
 
 def process_results():
+    plt.figure()
+    for agents in (1, 2, 4, 8):
+        episode_matrix = np.zeros((10, 2001))
+        for i in range(10):
+            with open('result-update-Partial-env-Taxi-v2-agents-{}-round-{}.json'.format(agents, i), 'r') as f:
+            # with open('result-update-Partial-env-Taxi-v2-agents-1-round-{}.json'.format(i), 'r') as f:
+            # with open('result-update-Partial-env-Taxi-v2-agents-2-round-{}.json'.format(i), 'r') as f:
+            # with open('result-update-Partial-env-Taxi-v2-agents-4-round-{}.json'.format(i), 'r') as f:
+            # with open('result-update-Partial-env-Taxi-v2-agents-8-round-{}.json'.format(i), 'r') as f:
 
-    episode_matrix = np.zeros((10, 2001))
-    for i in range(10):
-        # with open('result-update-Partial-env-Taxi-v2-agents-1-round-{}.json'.format(i), 'r') as f:
-        # with open('result-update-Partial-env-Taxi-v2-agents-2-round-{}.json'.format(i), 'r') as f:
-        # with open('result-update-Partial-env-Taxi-v2-agents-4-round-{}.json'.format(i), 'r') as f:
-        with open('result-update-Partial-env-Taxi-v2-agents-8-round-{}.json'.format(i), 'r') as f:
+                state = json.load(f)
+                reward_list = state['experimental_results']['results']['0']['cumulative_reward']
 
-            state = json.load(f)
-            reward_list = state['experimental_results']['results']['0']['cumulative_reward']
+                x = np.array(reward_list)
 
-            x = np.array(reward_list)
+                episode_matrix[i] = x
 
-            episode_matrix[i] = x
+        # avg_cumulative = episode_matrix.sum(axis=0)
+        # avg_cumulative = avg_cumulative/len(episode_matrix)
 
-    # avg_cumulative = episode_matrix.sum(axis=0)
-    # avg_cumulative = avg_cumulative/len(episode_matrix)
+        means = np.mean(episode_matrix, axis=0)
 
-    means = np.mean(episode_matrix, axis=0)
+        standard_errors = scipy.stats.sem(episode_matrix, axis=0)
+        # print(standard_errors)
 
-    standard_errors = scipy.stats.sem(episode_matrix, axis=0)
-    # print(standard_errors)
+        a = len(episode_matrix[0])
+        # conf = standard_errors * sp.stats.t._ppf((1+.95)/2., a-1)
 
-    a = len(episode_matrix[0])
-    # conf = standard_errors * sp.stats.t._ppf((1+.95)/2., a-1)
+        # print(conf)
 
-    # print(conf)
-
-    # uperconf = means + conf
-    # lowerconf = means - conf
-    uperconf = means + standard_errors
-    lowerconf = means - standard_errors
-
-
-
-    # print(*avg_cumulative.tolist(), sep="\n")
+        # uperconf = means + conf
+        # lowerconf = means - conf
+        uperconf = means + standard_errors
+        lowerconf = means - standard_errors
 
 
-    x = np.arange(0, len(means))
-    # plt.plot(x, means, 'o')
 
-    z = np.polyfit(x, means, 5)
-    p = np.poly1d(z)
+        # print(*avg_cumulative.tolist(), sep="\n")
 
-    plt.plot(x, p(x), 'r--')
+
+        x = np.arange(0, len(means))
+        # plt.plot(x, means, 'o')
+
+        z = np.polyfit(x, means, 5)
+        p = np.poly1d(z)
+
+        plt.plot(x, p(x), label=str(agents))
+
+        plt.fill_between(x, uperconf, lowerconf, alpha=0.3, antialiased=True)
 
     plt.ylim(ymax=50, ymin=-800)
 
-
-    plt.fill_between(x, uperconf, lowerconf, alpha=0.3, antialiased=True)
-
-
+    plt.savefig('test.svg')
     plt.show()
     plt.close()
 
