a61f46be11931fc87e90a7ff36186e51e1c709fc
MaxRobinson <maxrgit@gmail.com>
2018-04-28 10:31:31 -0400

    Update gather results to include modifying tau

--- DistQL/results/gatherAndWriteResults.py
+++ DistQL/results/gatherAndWriteResults.py
@@ -7,28 +7,30 @@ import requests
 SERVER_URL = 'http://localhost:5000'
 
 
-def get_and_save_data(num_agents, env_name, round):
-    print('Saving Experiment Data for Agents:{}, env:{}, round{}'.format(num_agents, env_name, round))
+def get_and_save_data(num_agents, env_name, round, update_freq):
+    print('Saving Experiment Data for Agents:{}, env:{}, round: {}, tau:{}'.format(num_agents, env_name, round, update_freq))
 
     r = requests.get('http://localhost:5000/state')
 
     state = r.json()
 
     # write to file
-    with open('result-update-{}-env-{}-agents-{}-round-{}.json'.format('Partial', env_name, num_agents, round), 'w') as f:
+    with open('result-tau-{}-update-{}-env-{}-agents-{}-round-{}.json'.format(update_freq, 'Partial', env_name, num_agents, round), 'w') as f:
         json.dump(state, f, indent=4)
 
     return
 
 
-def experiment(num_agents, env_name):
+def experiment(num_agents, env_name, update_frequency):
 
     for i in range(10):
         print('Start Experiment round: {}'.format(i))
 
-        r = requests.post(SERVER_URL+"/experiment/start", json={"num_agents": num_agents, "env_name": env_name})
+        r = requests.post(SERVER_URL+"/experiment/start", json={"num_agents": num_agents,
+                                                                "env_name": env_name,
+                                                                "update_freq": update_frequency})
 
-        time.sleep(10)
+        time.sleep(3)
         # is_complete = False
         while True:
             r = requests.get(SERVER_URL+"/experiment/status")
@@ -38,7 +40,7 @@ def experiment(num_agents, env_name):
             else:
                 time.sleep(1)
 
-        get_and_save_data(num_agents, env_name, i)
+        get_and_save_data(num_agents, env_name, i, update_frequency)
 
         requests.get(SERVER_URL+"/state/clear")
         requests.get(SERVER_URL+"/q/clear")
@@ -47,8 +49,23 @@ def experiment(num_agents, env_name):
 
 
 if __name__ == '__main__':
-    experiment(1, 'CartPole-v1')
-    experiment(2, 'CartPole-v1')
-    experiment(4, 'CartPole-v1')
-    experiment(8, 'CartPole-v1')
+    experiment(1, 'Taxi-v2', 50)
+    experiment(2, 'Taxi-v2', 50)
+    experiment(4, 'Taxi-v2', 50)
+    experiment(8, 'Taxi-v2', 50)
 
+    experiment(1, 'Taxi-v2', 100)
+    experiment(2, 'Taxi-v2', 100)
+    experiment(4, 'Taxi-v2', 100)
+    experiment(8, 'Taxi-v2', 100)
+
+    # CartPole with adjusted Tau
+    experiment(1, 'CartPole-v1', 50)
+    experiment(2, 'CartPole-v1', 50)
+    experiment(4, 'CartPole-v1', 50)
+    experiment(8, 'CartPole-v1', 50)
+
+    experiment(1, 'CartPole-v1', 100)
+    experiment(2, 'CartPole-v1', 100)
+    experiment(4, 'CartPole-v1', 100)
+    experiment(8, 'CartPole-v1', 100)
